5effb558db5a394f6053965e0b4a046a980913c7
parasSolanki <parassolanki151@gmail.com>
2023-02-15 19:58:06 +0530

    wip: code editor and format

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from '@/assets/react.svg'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="flex h-screen w-full space-x-4 p-2">
+      <div className="w-6/12">
+        <textarea
+          name="editor"
+          id="editor"
+          cols={30}
+          rows={10}
+          className="h-full w-full rounded border-2 border-slate-200 bg-black p-2 text-white"
+        ></textarea>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="w-6/12">
+        <iframe
+          src=""
+          className="h-full w-full rounded border-2 border-slate-400"
+        ></iframe>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
