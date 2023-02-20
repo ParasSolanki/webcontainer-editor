6fca7e10215c394b00b4e3d051cb71815b7d7e92
parasSolanki <parassolanki151@gmail.com>
2023-02-20 19:26:49 +0530

    add: show container url

--- src/App.tsx
+++ src/App.tsx
@@ -60,6 +60,7 @@ const detectLanguage = (filename: string) => {
 
 function App() {
   const [isLoading, setIsLoading] = useState(false);
+  const [url, setUrl] = useState("");
   const [output, setOutput] = useState("");
   const iframeRef = useRef<HTMLIFrameElement>(null);
   const editorRef = useRef<EditorType>(null);
@@ -106,6 +107,7 @@ function App() {
       // Wait for `server-ready` event
       webContainerInstanceRef.current.on("server-ready", (port, url) => {
         if (iframeRef.current) iframeRef.current.src = url;
+        setUrl(url);
         setIsLoading(false);
       });
     }
@@ -128,7 +130,11 @@ function App() {
 
       if (!value) return;
 
+      setIsLoading(true);
+
       await webContainerInstanceRef.current?.fs.writeFile("/index.js", value);
+
+      setIsLoading(false);
     });
   }
 
@@ -137,10 +143,30 @@ function App() {
       <div className="w-6/12">
         <Editor onMount={handleEditorOnMount} defaultLanguage="javascript" />
       </div>
-      <div className="w-6/12">
+      <div className="relative w-6/12">
+        <div className="absolute inset-x-0 top-0 flex h-10 items-center space-x-4 rounded bg-black p-2 px-3">
+          <button
+            type="button"
+            className="rounded-full p-1.5 text-white hover:bg-white/20"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fill="currentColor"
+                d="M19.91 15.51h-4.53a1 1 0 0 0 0 2h2.4A8 8 0 0 1 4 12a1 1 0 0 0-2 0a10 10 0 0 0 16.88 7.23V21a1 1 0 0 0 2 0v-4.5a1 1 0 0 0-.97-.99ZM12 2a10 10 0 0 0-6.88 2.77V3a1 1 0 0 0-2 0v4.5a1 1 0 0 0 1 1h4.5a1 1 0 0 0 0-2h-2.4A8 8 0 0 1 20 12a1 1 0 0 0 2 0A10 10 0 0 0 12 2Z"
+              ></path>
+            </svg>
+          </button>
+          <div className="flex h-6 flex-grow items-center rounded bg-white p-1 shadow">
+            <p className="text-sm text-black">{url}</p>
+          </div>
+        </div>
         <iframe
           ref={iframeRef}
-          className="h-full w-full rounded border-2 border-slate-400"
+          className="h-full w-full rounded border-2 border-slate-400 pt-9"
         ></iframe>
       </div>
     </div>
